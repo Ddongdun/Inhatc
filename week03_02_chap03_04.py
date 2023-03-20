462e49471102033c7d65e9c825c710215fb81a9e
Shindongjun <49547459+Ddongdun@users.noreply.github.com>
2023-03-20 16:11:20 +0900

    Update week03_02_chap03_04.py

--- week03_02_chap03_04.py
+++ week03_02_chap03_04.py
@@ -1,22 +1,23 @@
-## 함수 선언 부분 ##
+# week03_02_chap03_04
+
 def add_data(friend):
     katok.append(None)
-    k_len = len(katok)
-    katok[k_len - 1] = friend
+    katok[len(katok) - 1] = friend
+
 
 def insert_data(position, friend):
     if position < 0 or position > len(katok):
         print("데이터를 삽입할 범위를 벗어났습니다.")
         return
 
-    katok.append(None)  # 빈칸 추가
-    k_len = len(katok)  # 배열의 현재 크기
+    katok.append(None)
 
-    for i in range(k_len - 1, position, -1):
+    for i in range(len(katok) - 1, position, -1):
         katok[i] = katok[i - 1]
         katok[i - 1] = None
 
-    katok[position] = friend  # 배열의 제일 뒤에 친구 추가
+    katok[position] = friend
+
 
 def delete_data(position):
     if position < 0 or position > len(katok):
@@ -32,13 +33,10 @@ def delete_data(position):
 
     del (katok[k_len - 1])
 
-## 전역 변수 선언 부분 ##
-katok = []
-menu = -1  # 1: 추가, 2: 삽입, 3: 삭제, 4: 종료
 
-## 메인 코드 부분 ##
+katok = list()
+
 if __name__ == "__main__":
-
     while True:
         menu = input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> ")
 
@@ -46,22 +44,17 @@ if __name__ == "__main__":
             data = input("추가할 데이터--> ")
             add_data(data)
             print(katok)
-
         elif menu == "2":
             pos = int(input("삽입할 위치--> "))
             data = input("추가할 데이터--> ")
             insert_data(pos, data)
             print(katok)
-
         elif menu == "3":
             pos = int(input("삭제할 위치--> "))
             delete_data(pos)
             print(katok)
-
         elif menu == "4":
             print(katok)
-            #exit
             break
         else:
             print("1~4 중 하나를 입력하세요.")
-            #continue
