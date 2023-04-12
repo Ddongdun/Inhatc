a8fd603a4027695f78464e7a550b46fec55c27f9
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-12 19:22:30 +0900

    Add files via upload

---
+++ Week4_homework/Week4_homework/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Week4_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Num1 = new System.Windows.Forms.TextBox();
+            this.Num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_plus = new System.Windows.Forms.Button();
+            this.btn_minus = new System.Windows.Forms.Button();
+            this.btn_multiply = new System.Windows.Forms.Button();
+            this.btn_divide = new System.Windows.Forms.Button();
+            this.label_result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Num1
+            // 
+            this.Num1.Location = new System.Drawing.Point(240, 52);
+            this.Num1.Name = "Num1";
+            this.Num1.Size = new System.Drawing.Size(130, 21);
+            this.Num1.TabIndex = 0;
+            this.Num1.TextChanged += new System.EventHandler(this.Num1_TextChanged);
+            // 
+            // Num2
+            // 
+            this.Num2.Location = new System.Drawing.Point(240, 92);
+            this.Num2.Name = "Num2";
+            this.Num2.Size = new System.Drawing.Size(129, 21);
+            this.Num2.TabIndex = 1;
+            this.Num2.TextChanged += new System.EventHandler(this.Num2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(66, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "피연산자1";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(67, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "피연산자2";
+            // 
+            // btn_plus
+            // 
+            this.btn_plus.Location = new System.Drawing.Point(44, 167);
+            this.btn_plus.Name = "btn_plus";
+            this.btn_plus.Size = new System.Drawing.Size(50, 39);
+            this.btn_plus.TabIndex = 4;
+            this.btn_plus.Text = "+";
+            this.btn_plus.UseVisualStyleBackColor = true;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
+            // 
+            // btn_minus
+            // 
+            this.btn_minus.Location = new System.Drawing.Point(128, 167);
+            this.btn_minus.Name = "btn_minus";
+            this.btn_minus.Size = new System.Drawing.Size(50, 39);
+            this.btn_minus.TabIndex = 5;
+            this.btn_minus.Text = "-";
+            this.btn_minus.UseVisualStyleBackColor = true;
+            this.btn_minus.Click += new System.EventHandler(this.btn_minus_Click);
+            // 
+            // btn_multiply
+            // 
+            this.btn_multiply.Location = new System.Drawing.Point(214, 167);
+            this.btn_multiply.Name = "btn_multiply";
+            this.btn_multiply.Size = new System.Drawing.Size(50, 39);
+            this.btn_multiply.TabIndex = 6;
+            this.btn_multiply.Text = "*";
+            this.btn_multiply.UseVisualStyleBackColor = true;
+            this.btn_multiply.Click += new System.EventHandler(this.btn_multiply_Click);
+            // 
+            // btn_divide
+            // 
+            this.btn_divide.Location = new System.Drawing.Point(307, 167);
+            this.btn_divide.Name = "btn_divide";
+            this.btn_divide.Size = new System.Drawing.Size(50, 39);
+            this.btn_divide.TabIndex = 7;
+            this.btn_divide.Text = "/";
+            this.btn_divide.UseVisualStyleBackColor = true;
+            this.btn_divide.Click += new System.EventHandler(this.btn_divide_Click);
+            // 
+            // label_result
+            // 
+            this.label_result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label_result.Location = new System.Drawing.Point(44, 243);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(313, 91);
+            this.label_result.TabIndex = 8;
+            this.label_result.Text = "label3";
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_result.Click += new System.EventHandler(this.label_result_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 377);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.btn_divide);
+            this.Controls.Add(this.btn_multiply);
+            this.Controls.Add(this.btn_minus);
+            this.Controls.Add(this.btn_plus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Num2);
+            this.Controls.Add(this.Num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Num1;
+        private System.Windows.Forms.TextBox Num2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_plus;
+        private System.Windows.Forms.Button btn_minus;
+        private System.Windows.Forms.Button btn_multiply;
+        private System.Windows.Forms.Button btn_divide;
+        private System.Windows.Forms.Label label_result;
+    }
+}
+
---
+++ Week4_homework/Week4_homework/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week4_homework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            double NumbP_1;
+            //피연산자1을 담을 변수
+            double NumbP_2;
+            //피연산자2를 담을 변수
+            bool result1 = double.TryParse(Num1.Text, out NumbP_1);
+            //TryParse로 변환할수있는지 체크
+            bool result2 = double.TryParse(Num2.Text, out NumbP_2);
+            //TryParse로 변환할수있는지 체크
+            if (result1 && result2)
+            //두개의 피연산자 모두 변환할 수 있다면
+            {
+                label_result.Text = (NumbP_1 + NumbP_2).ToString();
+                //레이블의 텍스트에 결과값을 집어넣는다
+            }
+            else
+            //그렇지 않다면
+            {
+                label_result.Text = "형식이 안맞음";
+                //형식이 맞이 않음을 알려준다
+            }
+
+        }
+
+        private void Num1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_result_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            double NumbM_1;
+            //피연산자1을 담을 변수
+            double NumbM_2;
+            //피연산자2를 담을 변수
+            bool result1 = double.TryParse(Num1.Text, out NumbM_1);
+            //TryParse로 변환할수있는지 체크
+            bool result2 = double.TryParse(Num2.Text, out NumbM_2);
+            //TryParse로 변환할수있는지 체크
+            if (result1 && result2)
+            //두개의 피연산자 모두 변환할 수 있다면
+            {
+                label_result.Text = (NumbM_1 - NumbM_2).ToString();
+                //레이블의 텍스트에 결과값을 집어넣는다
+            }
+            else
+            //그렇지 않다면
+            {
+                label_result.Text = "형식이 안맞음";
+                //형식이 맞이 않음을 알려준다
+            }
+        }
+
+        private void btn_multiply_Click(object sender, EventArgs e)
+        {
+            double NumbMu_1;
+            //피연산자1을 담을 변수
+            double NumbMu_2;
+            //피연산자2를 담을 변수
+            bool result1 = double.TryParse(Num1.Text, out NumbMu_1);
+            //TryParse로 변환할수있는지 체크
+            bool result2 = double.TryParse(Num2.Text, out NumbMu_2);
+            //TryParse로 변환할수있는지 체크
+            if (result1 && result2)
+            //두개의 피연산자 모두 변환할 수 있다면
+            {
+                label_result.Text = (NumbMu_1 * NumbMu_2).ToString();
+                //레이블의 텍스트에 결과값을 집어넣는다
+            }
+            else
+            //그렇지 않다면
+            {
+                label_result.Text = "형식이 안맞음";
+                //형식이 맞이 않음을 알려준다
+            }
+        }
+
+
+        private void btn_divide_Click(object sender, EventArgs e)
+        {
+            double NumbD_1;
+            //피연산자1을 담을 변수
+            double NumbD_2;
+            //피연산자2를 담을 변수
+            bool result1 = double.TryParse(Num1.Text, out NumbD_1);
+            //TryParse로 변환할수있는지 체크
+            bool result2 = double.TryParse(Num2.Text, out NumbD_2);
+            //TryParse로 변환할수있는지 체크
+            if (result1 && result2)
+            //두개의 피연산자 모두 변환할 수 있다면
+            {
+                label_result.Text = (NumbD_1 / NumbD_2).ToString();
+                //레이블의 텍스트에 결과값을 집어넣는다
+            }
+            else
+            //그렇지 않다면
+            {
+                label_result.Text = "형식이 안맞음";
+                //형식이 맞이 않음을 알려준다
+            }
+        }
+
+      
+    }
+}
