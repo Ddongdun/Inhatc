d6425f17cd358bdc48b1f63aee4c866fb23cb7af
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:47:27 +0900

    Update ForDouble.java

--- week5/src/week5/ForDouble.java
+++ week5/src/week5/ForDouble.java
@@ -2,8 +2,11 @@ package week5;
 
 public class ForDouble {
 	public static void main(String[] args) {
-		for(int i=0; i<3; i++) { //i = 1, 2, 3 
+		for(int i=0; i<3; i++) { 
+		//i = 1, 2, 3 
+		//바깥 포문이 1번 반복될 때 마다
 			for(int j=0; j<3; j++) {
+			//안쪽 포문은 3번 반복된다
 				System.out.println(" i = " + i + ", j = " + j);
 				/*
 				  	i = 0, j = 0
