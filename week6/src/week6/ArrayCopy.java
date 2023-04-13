9c19904a23a677c5035295c9781b6b42004d5ab1
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 20:23:41 +0900

    Update ArrayCopy.java

--- week6/src/week6/ArrayCopy.java
+++ week6/src/week6/ArrayCopy.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
 public class ArrayCopy {
 	public static void main(String[] args) {
 		int[] arr1 = {1, 2, 3, 4, 5};
+		//정수형 배열에 값을 넣는다
 		int[] arr2 = new int[7];
+		//7의 크기를 갖는 정수형 배열 선언
 		
 		//for문을 이용한 복사
 		for(int i = 0; i<arr1.length;i++) {
+		//arr1의 크기만큼 반복
 			arr2[i] = arr1[i];
+			//arr2에 arr1의 값을 하나씩 집어넣는다
 		}
 		for(int i = 0; i<arr2.length;i++) {
-			System.out.println(arr2[i] + " ");	
+		//arr2의 크기만큼 반복
+			System.out.println(arr2[i] + " ");
+			//집어넣은 값 출력
 		}
 		
 		System.out.println();
