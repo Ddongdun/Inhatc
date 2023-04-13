43cf8bc3baf31d5d4ccd84f1f81e341aba61d2bf
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:44:10 +0900

    Update ContinueEx.java

--- week5/src/week5/ContinueEx.java
+++ week5/src/week5/ContinueEx.java
@@ -3,8 +3,11 @@ package week5;
 public class ContinueEx {
 	public static void main(String[] args) {
 		for (int i = 1; i<=100; i++) {
+		//1부터 100보다 작거나 같을때까지 증가 
 			if(i%2 != 0) {
+			//i가 홀수라면
 				continue;
+				//다시 반복
 			}
 			System.out.println(i);
 		}
