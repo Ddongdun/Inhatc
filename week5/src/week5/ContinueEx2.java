2408feac1484bc67b743f79a535ac4e03319c794
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:45:04 +0900

    Update ContinueEx2.java

--- week5/src/week5/ContinueEx2.java
+++ week5/src/week5/ContinueEx2.java
@@ -3,12 +3,17 @@ package week5;
 public class ContinueEx2 {
 	public static void main(String[] args) {
 		int sum = 0;
+		//초기값 선언
 		
 		for(int i=1;i<=100;i++) {
+		//i를 1부터 100보다 작거나 같을때 까지 증가
 			if(i%3 == 0) {
+			//3의 배수라면
 				continue;
+				//계속 반복
 			}
 			sum += i;
+			//그렇지 않다면 
 		}
 		System.out.println("1~100까지 3의 배수를 제외한 합 : " + sum);
 		
