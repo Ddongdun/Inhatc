e3bde590703141c4d98bbc48759d975ad751b8a2
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:36:56 +0900

    Update Break.java

--- week5/src/week5/Break.java
+++ week5/src/week5/Break.java
@@ -3,13 +3,17 @@ package week5;
 public class Break {
 	public static void main(String[] args) {
 		while(true) {
+		//무한반복
 			int num = (int)(Math.random() * 10);
+			//0~10까지의 랜덤 숫자
 			
 			System.out.println("랜덤 숫자 : " + num);
 			
 			if(num == 7) {
+			//숫자로 7이 나오면
 				System.out.println("프로그램 종료");
 				break;
+				//
 			}
 		}
 	}
