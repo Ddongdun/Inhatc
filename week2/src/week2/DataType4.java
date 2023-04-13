1f2cbb6fc87929f13e222d61ed99929fce9c8bf2
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 18:22:19 +0900

    Update DataType4.java

--- week2/src/week2/DataType4.java
+++ week2/src/week2/DataType4.java
@@ -2,11 +2,16 @@ package week2;
 
 public class DataType4 {
 	public static void main(String[] args) {
-		byte bData = 127; //128바이트 보다 작은 값 0~127이 출력할 수 있는 범위
-		char cData = 90; //음수가 아닌 값
-		float fData = 3.14f; //float 타입으로 넣어준다
-		long lData = 1000000000L; // long 타입으로 넣어준다
-		boolean boolD = true; //true or false
+		byte bData = 127; //
+		128바이트 보다 작은 값 0~127이 출력할 수 있는 범위
+		char cData = 90; 
+		//음수가 아닌 값
+		float fData = 3.14f; 
+		//float 타입으로 넣어준다
+		long lData = 1000000000L; 
+		// long 타입으로 넣어준다
+		boolean boolD = true; 
+		//true or false
 		
 		System.out.println("bData = " + bData);
 		System.out.println("cData = " + cData);
