8209b11e38b98de4a1c7c5c67cbb8f1d0c85bda7
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:18:37 +0900

    Update IfNested.java

--- week4/src/week4/IfNested.java
+++ week4/src/week4/IfNested.java
@@ -3,27 +3,45 @@ package week4;
 public class IfNested {
 	public static void main(String[] args) {
 		int score = (int)(Math.random() * 100);
+		//0~100까지의 랜덤 숫자
 		String grade;
 		if (score >=90) { 
+		//스코어가 90이상이고
 			if (score >96) 
+			//96점 초과라면
 				grade = "A+";
+				//등급은 A+
 			
 			else if (score > 93) 
+			//93점 초과라면
 				grade = "A";
+				//등급은 A
 			
 			else 
+			//그 이외라면
 				grade = "A-";
+				//등급은 A-
 		}
 			else if (score >= 80) {
+			//스코어가 80이상이고
 				if (score > 86) 
+				//86점 초과라면
 					grade = "B+";
+					//등급은 B+
 				else if (score > 83)
+				//83점 초과라면
 					grade = "B";
+					//등급은 B
 				else
+				//그 이외라면
 					grade = "B-";
+					//등급은 B-
 			
-			} else
+			} 
+		else
+		//그 이하 점수라면
 				grade = "F";
+				//등급은 F
 		System.out.println(score + grade);
 	} 
 }
