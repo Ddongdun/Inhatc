a0166399cab70f10d691bd06d8f36b534bf73ab4
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:03:46 +0900

    Update PromotionType.java

--- week3/src/week3/PromotionType.java
+++ week3/src/week3/PromotionType.java
@@ -4,13 +4,18 @@ public class PromotionType {
 
 	public static void main(String[] args) {
 		byte bValue = 25;
+		//byte형식으로 25 선언
 		int iValue = bValue;
+		//byte를 int형 변수에 넣어준다
 		System.out.println(iValue);
 		//25
 		
 		char cValue = '가';
+		//char형식으로 가 선언
 		iValue = cValue;
+		//char를 int형 변수에 넣어준다
 		System.out.println(iValue);
+		//자동으로 int형으로 변환된다.
 		//44032
 		
 		iValue = 375;
