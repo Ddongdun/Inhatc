c2dc938256fb62ef0a6f376f785cde2fa5a0a21d
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 18:17:00 +0900

    Update DataType1.java

--- week2/src/week2/DataType1.java
+++ week2/src/week2/DataType1.java
@@ -3,15 +3,23 @@ package week2;
 public class DataType1 {
 	public static void main(String[] args) {
 		byte bData = 65;
+		//1byte
 		int iData = 65;
+		//4byte
 		short sData = 65;
+		//2byte
 		long lData = 65;
+		//8byte
 		float fData = 65.12f;
 		//float 데이터는 f를 붙여준다
+		//4byte
 		double dData = 65.12d;
 		//double 데이터는 d를 붙여준다
+		//8byte
 		char cData = 65;
+		//2byte
 		boolean boolData = true;
+		//1byte
 		
 		System.out.println("bData = " + bData);
 		System.out.println("iData = " + iData);
