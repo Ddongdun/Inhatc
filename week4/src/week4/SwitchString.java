669c2c0b6cd500084d38025dc71b32c370db4ed1
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:31:59 +0900

    Update SwitchString.java

--- week4/src/week4/SwitchString.java
+++ week4/src/week4/SwitchString.java
@@ -3,9 +3,12 @@ package week4;
 public class SwitchString {
 	public static void main(String[] args) {
 		String mobile = "iPhone";
+		//문자열 값 저장
 		switch(mobile) {
+		//mobile변수 값에 따른 case 작성
 		case "iPhone" -> System.out.println("애플");
 		case "Galaxy" -> System.out.println("삼성");
+		//->를 이용해 바로 출력
 		default -> System.out.println("기타");
 		}
 	}
