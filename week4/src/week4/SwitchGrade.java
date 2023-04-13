f672a446466b1c1d2331592aee0b7907cd3090a2
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:30:56 +0900

    Update SwitchGrade.java

--- week4/src/week4/SwitchGrade.java
+++ week4/src/week4/SwitchGrade.java
@@ -6,10 +6,13 @@ public class SwitchGrade {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("점수를 입력하세요(0~100) >>");
+		//점수를 입력받는다
 		
 		int score = sc.nextInt();
+		//점수를 정수형으로 입력받아 int형 변수에 저
 		
 		char grade = switch(score/10) {
+		//grade 변수에 switch문을 넣어서 값을 저장할 수 있다.
 		case 10, 9 -> 'A';
 		case 8 -> 'B';
 		case 7 -> 'C';
