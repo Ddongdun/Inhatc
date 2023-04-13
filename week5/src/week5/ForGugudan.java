ad6ef7ee8629c7fd826b9014fbee2dcf55e7e853
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:48:06 +0900

    Update ForGugudan.java

--- week5/src/week5/ForGugudan.java
+++ week5/src/week5/ForGugudan.java
@@ -3,10 +3,12 @@ package week5;
 public class ForGugudan {
 	public static void main(String[] args) {
 		for(int i = 2; i <= 9; i++) {
+		//구구단 2단부터 9단까지
 			System.out.println("==========");
 			System.out.println("구구단 : " + i +"단");
 			System.out.println("==========");
 			for(int j = 1; j <=9; j++) {
+			//*1부터 *9값 
 				System.out.println(i + " * " + j + " = " + (i * j));
 			}
 		}
