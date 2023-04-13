469d8cc88ca8ef8993cb4f9fba21f84023f28c1b
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 18:39:41 +0900

    Update CastingType.java

--- week3/src/week3/CastingType.java
+++ week3/src/week3/CastingType.java
@@ -3,24 +3,34 @@ package week3;
 public class CastingType {
 	public static void main(String[] args) {
 		int iData = 65;
+		//A의 아스키코드 값
 		char cData = (char) iData;
+		//char형식으로 형변환
 		System.out.println(cData);
 		//A
 		
 		long lData = 500;
+		//정수 500을 Long형으로 선언
 		iData = (int)lData;
+		//int형식으로 형변환
 		System.out.printf("iData = %d\n", iData);
 		//iData = 500
 		
 		double dData = 3.14;
+		//실수 3.14.를 double형으로 선언
 		iData = (int)dData;
+		//int형식으로 형변환
 		System.out.printf("iData = %d\n", iData);
 		//iData = 3
+		//실수를 정수로 형변환하여 소수점 표시x
 		
 		int iValue = 128;
+		//정수 128을 int형으로 선언
 		byte bValue = (byte)iValue;
+		//byte형식으로 형변
 		System.out.println("bValue = " + bValue);
 		//bValue = -128
+		//표현할 수 있는 범위를 벗어나 오버플로우
 		
 		if(iValue >= Byte.MIN_VALUE && iValue <= Byte.MAX_VALUE) {
 			bValue = (byte)iValue;
