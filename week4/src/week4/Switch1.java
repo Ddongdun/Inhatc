b31d648a07a2a41f305600dd86dee25e875f49eb
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:21:47 +0900

    Update Switch1.java

--- week4/src/week4/Switch1.java
+++ week4/src/week4/Switch1.java
@@ -6,16 +6,21 @@ public class Switch1 {
 		System.out.println("[현재 시간 : " + time + "시]");
 
 		switch (time) {
+		//time값에 따른 case
 		case 8:
+		//time값이 8일때
 			System.out.println("출근 중입니다");
 			break;
 		case 9:
+		//time값이 9일때
 			System.out.println("회의 시간입니다");
 			break;
 		case 10:
+		//time값이 10일때
 			System.out.println("근무 중입니다");
 			break;
 		default:
+		//그 이외의 값일
 			System.out.println("출장 중입니다");
 		}
 
