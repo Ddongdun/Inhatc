d3154dc3942a9cefb9f04c4ca9898c09af56cd18
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:57:14 +0900

    Update WhileEx1.java

--- week5/src/week5/WhileEx1.java
+++ week5/src/week5/WhileEx1.java
@@ -6,8 +6,11 @@ public class WhileEx1 {
 		int num = 1;
 		
 		while(num <= 100) {
+		//num이 100보다 같거나 작을때까지 반복
 			sum += num;
+			//sum에 합을 구한다
 			num ++;
+			//num1 
 		}
 		
 		System.out.println("1~100까지의 합 : " + sum);
