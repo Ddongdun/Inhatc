7260da6a78b5b659f1596622f26e6cd65b409e4d
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 13:20:52 +0900

    Update HelloWorld.java

--- week1/src/week1/HelloWorld.java
+++ week1/src/week1/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("학번 : 202244112");
 		System.out.println("학과 : 컴퓨터정보과");
-		System.out.println("이룸 : 신동준");
+		System.out.println("이름 : 신동준");
 		
 	}
 
