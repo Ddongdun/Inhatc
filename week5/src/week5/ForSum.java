2c9d3af76cb7cda90f1f4331b2a85a44b4eefae9
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:49:06 +0900

    Update ForSum.java

--- week5/src/week5/ForSum.java
+++ week5/src/week5/ForSum.java
@@ -5,7 +5,9 @@ public class ForSum {
 		int sum = 0;
 		
 		for(int i=1 ; i<=100 ; i++) 
+		//100보다 작거나 같을때 까지 반복
 			sum = sum + i;
+			//sum변수에 계속 더한다
 		System.out.println("1~100 까지의 합 : " + sum);
 		
 	}
