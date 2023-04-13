5a170ed1fda985d8b78bb8bf5432eb353b1c3c3d
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-13 19:58:22 +0900

    Update WhileEx2.java

--- week5/src/week5/WhileEx2.java
+++ week5/src/week5/WhileEx2.java
@@ -10,9 +10,11 @@ public class WhileEx2 {
 		while(true) {
 			System.out.println("첫 번째 수 입력 >>");
 			num1 = sc.nextInt();
+			//num1에 입력값 저장
 			
 			System.out.println("두 번째 수 입력 >>");
 			num2 = sc.nextInt();
+			//num2에 입력값 
 			
 			System.out.printf("%d + %d = %d\n", num1, num2, num1+num2);
 		}
