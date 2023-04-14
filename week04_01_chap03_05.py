45a8fab247a132133f1d608f9a35ee085239523a
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-14 17:58:01 +0900

    Update week04_01_chap03_05.py

--- week04_01_chap03_05.py
+++ week04_01_chap03_05.py
@@ -5,20 +5,30 @@ def print_poly(f_x):
     :param f_x: 정수 타입의 계수를 원소로 하는 리스트
     :return: 다항식 문자열
     """
-    term = len(f_x) - 1  # 최고차항 숫자 = 배열길이-1
-    poly_str = "P(x) = "  # 배열에 있는 값을 다항식으로 변환 후 출력
+    term = len(f_x) - 1  
+    # 최고차항 숫자 = 배열길이-1
+    poly_str = "P(x) = "  
+    # 다항식으로 변환하여 담을 변수
 
     for i in range(len(fx)):
-        coef = f_x[i]  # 계수
+        coef = f_x[i]  
+        # 계수
 
-        if coef >= 0:  # 계수가 0보다 크거나 같다면
-            #poly_str += "+"  # 부호를 붙여 양수로 만든다
-            poly_str = poly_str + "+"  # 부호를 붙여 양수로 만든다
-        #poly_str += str(coef) + "x^" + str(term) + " "  # 거듭제곱수를 붙여준다
+        if coef >= 0:  
+        # 계수가 0보다 크거나 같다면
+            #poly_str += "+"  
+            # 다른 표현
+            poly_str = poly_str + "+"  
+            # 부호를 붙여 양수로 만든다
+        #poly_str += str(coef) + "x^" + str(term) + " "  
+        # 거듭제곱수를 붙여준다
         poly_str = poly_str + f"{coef}x^{term} "
-        term = term - 1  # 다음 차항
+        # 다항식을 만들어준다
+        term = term - 1  
+        # 다음 차항
 
-    return poly_str  # 결과 값 반환
+    return poly_str  
+    # 결과 값 반환
 
 
 def calc_poly(x_Value, f_x):
@@ -29,18 +39,25 @@ def calc_poly(x_Value, f_x):
     :return: 다항식 산술 연산 계산 값
     """
     ret_value = 0
-    term = len(f_x) - 1  # 최고차항 숫자 = 배열길이-1
+    # 결과 값을 담을 변수
+    term = len(f_x) - 1  
+    # 최고차항 숫자 = 배열길이-1
 
     for i in range(len(fx)):
-        coef = f_x[i]  # 계수
-        ret_value += coef * x_Value ** term  # 항의 값 계산해서 변수에 더해준다
-        term = term - 1  # 다음 차항
+        coef = f_x[i]  
+        # 계수
+        ret_value += coef * x_Value ** term  
+        # 항의 값 계산해서 변수에 더해준다
+        term = term - 1  
+        # 다음 차항
 
     return ret_value
+    #결과 값 반환
 
 
 ## 전역 변수 선언 부분 ##
-fx = [7, -4, 0, 5]  # = 7x^3 -4x^2 +0x^1 +5x^0
+fx = [7, -4, 0, 5]  
+# = 7x^3 -4x^2 +0x^1 +5x^0
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
