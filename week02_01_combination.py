22a0c62d8bbcbdd72456f1fe2e317e64703cbce5
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-14 16:42:32 +0900

    Update week02_01_combination.py

--- week02_01_combination.py
+++ week02_01_combination.py
@@ -5,9 +5,13 @@ def factorial(n):
     :return: 정수, n!
     """
     result = 1
+    //초깃값 1
     for i in range(2, n+1):
+    //2부터 n+1보다 작을때까지 반복
         result = result * i
+        //곱한다
     return result
+    //결과 값 반환
 
 def nCr(n, r) -> int:
     """
@@ -17,8 +21,11 @@ def nCr(n, r) -> int:
     :return: 경우의 수
     """
     denominator = factorial(n-r) * factorial(r)
+    //분모 구하는 공식
     numerator = factorial(n)
+    //분자 구하는 공식
     return int(numerator / denominator)
+    //경우의 수 구하는 공식 반환
 
 help(nCr)
 #함수 설명 출력
