12057fec6cc826a74565673af8b6bc374680fba7
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-14 17:50:09 +0900

    Update week03_02_chap03_04.py

--- week03_02_chap03_04.py
+++ week03_02_chap03_04.py
@@ -2,36 +2,53 @@
 
 def add_data(friend):
     katok.append(None)
+    #배열 맨 뒤 빈칸 추가
     katok[len(katok) - 1] = friend
+    #배열 맨 뒤에 빈칸을 추가해 길이가 1씩 늘어났으므로 -1을 해서 넣어준다
 
 
 def insert_data(position, friend):
     if position < 0 or position > len(katok):
+    #넣을 위치가 0보다 작고 배열보다 크면
         print("데이터를 삽입할 범위를 벗어났습니다.")
         return
 
     katok.append(None)
+    #배열 맨 뒤 빈칸 추가
 
     for i in range(len(katok) - 1, position, -1):
+    #배열 맨 뒤에 빈칸을 추가했으므로 -1만큼 시작, 원하는 자리가 나올때까지 -1씩 줄이면서 반복
         katok[i] = katok[i - 1]
+        #뒤로 한칸씩 땡겨준다
         katok[i - 1] = None
+        #땡기고 빈 자리는 남긴다
 
     katok[position] = friend
+    #원하는 자리에 넣는다
+    #맨 끝에 데이터가 들어갈 경우 포문은 동작하지 않는다
 
 
 def delete_data(position):
     if position < 0 or position > len(katok):
+    #삽입할 위치가 0보다 작고 배열보다 크다면
         print("데이터를 삭제할 범위를 벗어났습니다.")
         return
 
     k_len = len(katok)
-    katok[position] = None  # 데이터 삭제
+    #배열의 길이 저장
+    katok[position] = None  
+    #삭제를 원하는 곳의 데이터 삭제
 
     for i in range(position + 1, k_len):
+    #삭제된 데이터의 다음위치부터 배열의 크기만큼 반복
         katok[i - 1] = katok[i]
-        katok[i] = None  # 배열의 제일 위치 삭제
+        #앞으로 한칸씩 당겨준다
+        katok[i] = None  
+        #남는 칸은 빈칸으로 남겨둔다
+        
 
     del (katok[k_len - 1])
+    #맨 마지막 칸 삭제
 
 
 katok = list()
