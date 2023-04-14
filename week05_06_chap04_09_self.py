755fb1680ed9285096c1fbfede762a090b975a4b
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-14 20:49:48 +0900

    Update week05_06_chap04_09_self.py

--- week05_06_chap04_09_self.py
+++ week05_06_chap04_09_self.py
@@ -13,7 +13,7 @@ def print_nodes(start):
 
     print(current.data, end=' ')
     while current.link is not None:
-        # current의 링크 값이 None이 아닐때 까지
+        # 노드 데이터가 없을때까지
         current = current.link
         # 가르키는 대상 증가
         print(current.data, end=' ')
@@ -21,17 +21,25 @@ def print_nodes(start):
 
 def make_simple_linked_List(name_height) :
 	global head, current, pre
+	#전역 변수 선언
 	print_nodes(head)
 
 	node = Node()
+	#새 노드 생성
 	node.data = name_height
-	if head is None:			# 첫 번째 노드일 때
+	#새 노드에 데이터 삽입
+	if head is None:
+	#헤드가 없다면
 		head = node
+		#새 노드를 헤드로 지정
 		return
 
-	if head.data[1] > name_height[1] :	# 첫 번째 노드보다 작을 때
+	if head.data[1] > name_height[1] :	
+	#헤드의 두번째 데이터가 넣을 두번째 데이터보다 크다면
 		node.link = head
+		#새 노드의 링크를 헤드로 지정
 		head = node
+		#새 노드를 헤드로 지정
 		return
 
 	# 중간 노드로 삽입하는 경우
@@ -39,16 +47,20 @@ def make_simple_linked_List(name_height) :
 	while current.link != None :
 	#빈 값이 나올때까지 반복
 		pre = current
+		#전 노드
 		current = current.link
+		#후 노드
 		if current.data[1] > name_height[1]:
+		#현재 노드의 두번째 데이터가 넣을 두번째 데이터보다 크다면
 			pre.link = node
-			#이후 데이터의 링크 연결
+			#전 노드의 링크를 새 노드와 연결
 			node.link = current
-			#노드의 링크에 이전 데이터 연결
+			#새 노드의 링크를 후 노드와 연결
 			return
 
 	# 삽입하는 노드가 가장 큰 경우
 	current.link = node
+	#맨 뒤에 연결
 
 ## 전역 변수 선언 부분 ##
 head, current, pre = None, None, None
