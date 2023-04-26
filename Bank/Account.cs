23729a9520ab7c63d19597a682589e3829ad3056
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-04-26 14:29:47 +0900

    Add files via upload

---
+++ Bank/Account.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Account
+    {
+        private static int AccountCount = 0;
+        //계좌 개설 개수, 이를 이용해 자동 계좌번호 생성
+        private string AccountNumber;
+        //계좌번호
+        private string Owner;
+        //소유자
+        private long Balance;
+        //잔액
+
+        public string GetAccountNumber()
+        //계좌번호 가져오는 메소드
+        {
+            return this.AccountNumber;
+            //계좌번호 반환
+        }
+
+        public string GetOwner()
+        //소유자 이름 가져오는 메소드
+        {
+            return this.Owner;
+            //소유자 이름 반환
+        }
+
+        public void SetOwner(string name)
+        //소유자 이름 변경하는 메소드
+        {
+            this.Owner = name;
+            //변경된 소유자 이름 반환
+        }
+
+        public long GetBalance()
+        //계좌 잔액 가져오는 메소드
+        {
+            return this.Balance;
+            //계좌 잔액 반환
+        }
+
+        public bool Deposit(long amount)
+        //입금 메소드
+        {
+            this.Balance += amount;
+            //계좌 잔액에 입금할 금액을 더한다
+            return true;
+            //입금 성공
+        }
+
+        public bool Withdraw(long amount)
+        //출금 메소드
+        {
+            if(this.Balance < amount)
+            //계좌 잔액이 출금할 금액보다 적다면
+            {
+                return false;
+                //출금 실패
+            }
+            this.Balance -= amount;
+            //계좌에서 출금한다
+            return true;
+            //출금 성공
+        }
+
+        public Account(string name)
+        {
+            AccountCount++;
+            this.AccountNumber = AccountCount.ToString("000000");
+            this.Owner = name;
+            //this.Balance = 0;
+        }
+        public Account(string name, long amount)
+            : this(name)
+        {
+            //AccountCount++;
+            //this.AccountNumber = AccountCount.ToString("000000");
+            //this.Owner = name;
+            //this.Balance = 0;
+        }
+
+        public string Info()
+        {
+            string result = $"계좌번호: {AccountNumber}";
+            result += Environment.NewLine;
+            result += $"소유주: {Owner}";
+            result += Environment.NewLine;
+            result += $"잔액: {Balance}";
+            result += Environment.NewLine;
+            return result;
+        }
+    }
+}
---
+++ Bank/BankSystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class BankSystem
+    {
+        static List<Account> accounts = new List<Account>();
+        //계좌를 저장할 리스트를 인스턴스로 선언
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 2; i++)
+            //계좌 신규 개설
+            {
+                var name = Console.ReadLine();
+                //이름 입력받기
+                var amount = Console.ReadLine();
+                //금액 입력받기
+
+                Account acc;
+                //계좌 생성
+                if (string.IsNullOrEmpty(amount))
+                //입금액이 없다면
+                {
+                    acc = new Account(name);
+                    //이름만 넣어서 계좌 생성
+                }
+                else
+                {
+                    long initmoney = 0;
+                    //계좌 잔액
+                    if(long.TryParse(amount, out initmoney))
+                    //long 타입으로 형변환이 잘 되었다면
+                    {
+                        acc = new Account(name, initmoney);
+                        //이름과 잔액을 넣어서 계좌 생성
+                    } else
+                      //안되었다면
+                    {
+                        acc = new Account(name);
+                        //이름만 넣어서 계좌 생성
+                    }
+                }
+                accounts.Add(acc);
+                //리스트에 만든 계좌 추가
+            }
+
+            Console.ReadLine();
+
+            if (accounts[0].Deposit(1000000))
+            //0번째 계좌에 입금
+            {
+                Console.WriteLine("입금성공");
+                Console.WriteLine(accounts[0].Info());
+            }
+
+            Console.ReadLine();
+
+            if (accounts[1].Withdraw(1000000))
+            //1번째 계좌에서 출금
+            {
+                Console.WriteLine("출금성공");
+            }
+            else
+            {
+                Console.WriteLine("출금실패");
+                Console.WriteLine($"잔액부족:{accounts[1].GetBalance()}");
+            }
+
+            Console.ReadLine();
+
+            foreach(var acc in accounts)
+            //계좌 수 만큼 반복
+            {
+                Console.WriteLine(acc.Info());
+                //계좌 정보 출력
+            }
+        }
+    }
+}
