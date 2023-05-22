3a1521b1d32abb3f3aa8496006f211cc6d380271
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-22 13:17:03 +0900

    Update week11_03_chap08_02.py

--- week11_03_chap08_02.py
+++ week11_03_chap08_02.py
@@ -38,27 +38,45 @@ node4.left = node8
 
 
 def preorder(node):
+#전위 순회
     if node is None:
+    #노드가 비었다면
         return
+        #리턴
     print(node.data, end='->')
+    #노드 데이터 출력
     preorder(node.left)
+    #왼쪽 데이터 진입
     preorder(node.right)
+    #오른쪽 데이터 진입
 
 
 def inorder(node):
+#중위 순회
     if node is None:
+    #노드가 비었다면
         return
+        #리턴
     inorder(node.left)
+    #왼쪽 데이터 진입
     print(node.data, end='->')
+    #노드 데이터 출력
     inorder(node.right)
+    #오른쪽 데이터 진입
 
 
 def postorder(node):
+#후위 순회
     if node is None:
+    #노드가 비었다면
         return
+        #리턴
     postorder(node.left)
+    #왼쪽 데이터 진입
     postorder(node.right)
+    #오른쪽 데이터 진입
     print(node.data, end='->')
+    #노드 데이터 출력
 
 
 print(node1.data, end=' ')
