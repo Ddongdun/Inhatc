d502de237828abf03203f3f2406cef7ab1d57b24
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-22 13:25:43 +0900

    Update week09_02_chap06_04_self.py

--- week09_02_chap06_04_self.py
+++ week09_02_chap06_04_self.py
@@ -1,10 +1,17 @@
 def push(data) :
+#데이터를 넣는 함수
     global SIZE, stack, top
+    #전역 변수선언 
     if top == SIZE-1:
+    #탑이 사이즈 = 1과 같다면 
         print("스택이 꽉 찼습니다.")
+        #알려준다
         return
+        #리턴 x
     top = top + 1
+    #탑 값 + 1
     stack[top] = data
+    #탑에 데이터를 넣는다 
 
 SIZE = 5
 stack = ["커피", "녹차", "꿀물", "콜라", None]
