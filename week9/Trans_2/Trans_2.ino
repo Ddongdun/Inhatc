cfcfe842dae799a9155d51d63b975ed21cf7c0fc
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-23 20:10:14 +0900

    Update Trans_2.ino

--- week9/Trans_2/Trans_2.ino
+++ week9/Trans_2/Trans_2.ino
@@ -7,7 +7,9 @@ void setup()
 void loop() 
 { 
   int sensorValue = analogRead(A0);
+  //A0포트를 이용해 아날로그 값을 입력받는다
   int ledBrightness = map(sensorValue, 0, 1023, 0, 255);
   //센서로 읽어온 값을 map함수를 이용해 LED의 밝기로 변경한다
   analogWrite(LED_PIN, ledBrightness); 
+  //LED_PIN포트의 전압을 ledBrightness만큼 설정
 }
