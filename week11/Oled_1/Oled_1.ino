90df36e8d5d72b9f1bacbe48222e33dbd42d5435
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-23 20:44:38 +0900

    Update Oled_1.ino

--- week11/Oled_1/Oled_1.ino
+++ week11/Oled_1/Oled_1.ino
@@ -8,17 +8,25 @@
 #define OLED_RESET 4
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+//adafruit 라이브러리로 display 설정
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  
+  //디스플레이 초기화
 
 }
 
 void loop() {
   display.clearDisplay();
+  //디스플레이 비움
   display.setTextSize(1);
+  //디스플레이 글씨 크기 설정
   display.setTextColor(WHITE);
+  //디스플레이 폰트 색 설정
   display.setCursor(0,0);
+  //커서 위치 설정
   display.println("INHATC");
+  //출력
   display.display();
+  //디스플레이에 표시
 }
