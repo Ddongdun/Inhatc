ae6862c9662684e6c7b653c8341a09117222f4e0
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-23 20:51:59 +0900

    Update Step_motor_1.ino

--- week12/Step_motor_1/Step_motor_1.ino
+++ week12/Step_motor_1/Step_motor_1.ino
@@ -3,15 +3,20 @@
 #define IN2 9
 #define IN3 10
 #define IN4 11
-//360˚ 회전
+//핀 설정
 const int stepsPerRevolution = 2048; 
+//360˚ 회전
 Stepper myStepper(stepsPerRevolution,IN4,IN2,IN3,IN1);
+//Stepper 라이브러리 설정
 void setup() {
   myStepper.setSpeed(14); 
+  //속도 설정
 }
 void loop() {
-  myStepper.step(stepsPerRevolution); //시계 반대방향
+  myStepper.step(stepsPerRevolution); 
+  //시계 반대방향
   delay(500);
-  myStepper.step(-stepsPerRevolution); //시계 방향
+  myStepper.step(-stepsPerRevolution); 
+  //시계 방향
   delay(500);
 }
