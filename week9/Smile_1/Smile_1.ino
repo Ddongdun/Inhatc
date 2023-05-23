6c7a9003fce54289b72fcfd25cd373b2752a06ac
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-23 20:08:59 +0900

    Update Smile_1.ino

--- week9/Smile_1/Smile_1.ino
+++ week9/Smile_1/Smile_1.ino
@@ -8,7 +8,9 @@
 {0,1,0,0,0,0,1,0}, \
 {0,0,1,1,1,1,0,0} \
 }
+//얼굴을 표현할 배열
 byte col = 0;
+//column 변수
 byte leds[8][8];
 
 int pins[17]= {-1, 5, 4, 3, 2, 14, 15, 16, 17, 13, 12, 11, 10, 9, 8, 7, 6};
@@ -26,42 +28,51 @@ void setup() {
   for(int i = 1;i<=16;i++){
     //차례로 출력모드 설정
     pinMode(pins[i], OUTPUT);
+    //핀 모드를 출력으로 바꿈
   }
 
   for(int i =1;i<=8;i++){
     //초기화
     digitalWrite(cols[i-1], LOW);
+    //콜 핀을 Low로 변경
   }
 
   for(int i =1;i<=8;i++){
     //초기화
     digitalWrite(rows[i-1], LOW);
+    //로우 핀을 Low로경변경
   }
 
 }
 
 void loop() {
   display();
+  //display 함수 반복
   delay(2);
+  //2ms 딜레이
 
 }
 
 void display(){
-  //다음 column으로 넘어가기 전에 이전led를 끈다
   digitalWrite(cols[col], LOW);
-  //현재 column
+  //다음 column으로 넘어가기 전에 이전led를 끈다
   col++;
+  //현재 column
   if (col == 8) {
+  //column이 끝까지 증가했다면
   col = 0;
+  //0으로 초기화
   }
   for (int row = 0; row < 8; row++) {
     //한 줄씩 키는 반복문
     if (pattern[col][7 - row] == 1) {
     //패턴에 저장된 배열 값이 1이라면
-    digitalWrite(rows[row], LOW); // LED ON
+    digitalWrite(rows[row], LOW); 
+    // LED ON
     }
     else {
-    digitalWrite(rows[row], HIGH); // LED OFF
+    digitalWrite(rows[row], HIGH); 
+    // LED OFF
     }
   }
     digitalWrite(cols[col], HIGH);
