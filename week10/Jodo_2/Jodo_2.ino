1e37d6995b6da3d8b459fb2800b8c21c93324324
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-23 20:17:20 +0900

    Update Jodo_2.ino

--- week10/Jodo_2/Jodo_2.ino
+++ week10/Jodo_2/Jodo_2.ino
@@ -7,14 +7,21 @@ void setup()
 void loop()
 {
   int luminance = analogRead(A0);
+  //A0으로 아날로그 값을 입력받는다
   Serial.println(luminance);
+  //저장받은 값 출력
   if(luminance >= 400)
+  //밝기가 400이상이라면
   {
     digitalWrite(LED_PIN, HIGH);
+    //LED를 켬
   }
   else
+  //그렇지 않다면
   {
     digitalWrite(LED_PIN, LOW);
+    //LED를 끈다
   }
   delay(1000);
+  //1초 딜레이
 }
