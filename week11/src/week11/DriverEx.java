34e7493c5b066b30048bde3cda7c5bf7704e458c
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 20:48:33 +0900

    Update DriverEx.java

--- week11/src/week11/DriverEx.java
+++ week11/src/week11/DriverEx.java
@@ -3,8 +3,11 @@ package week11;
 public class DriverEx {
 	public static void main(String[] args) {
 		Driver d = new Driver();
+		//Driver 클래스 생성자 선언
 		Vehicle v = new Vehicle();
+		//Vehicle 클래스 생성자 선언
 		d.drive(v);
+		//운전 메소드 실행
 		
 		Bus bus = new Bus();
 		d.drive(bus);
