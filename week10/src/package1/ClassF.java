2750bd225bf20e293a71f05018c85bad0e98c3f7
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:58:26 +0900

    Update ClassF.java

--- week10/src/package1/ClassF.java
+++ week10/src/package1/ClassF.java
@@ -4,11 +4,17 @@ public class ClassF {
 	public ClassF() {
 		ClassD d1 = new ClassD();
 		d1.field1 = 10;
+		//public 필드
 		//d1.field2 = 10;
+		//default 필드는 같은 패키지에서 접근 가능
 		//d1.field3 = 10;
+		//private 필드는 같은 클래스내에서 접근 가능
 		
 		d1.method1();
+		//public 메소드
 		//d1.method2();
+		//default 메소드는 같은 패키지에서 접근 가능
 		//d1.method3();
+		//private 메소드는 같은 클래스내에서 접근 가능
 	}
 }
