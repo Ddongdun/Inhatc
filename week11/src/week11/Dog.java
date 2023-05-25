25ad56f53efc1a10919ebd2af9a9cc396d5c5234
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 20:37:47 +0900

    Update Dog.java

--- week11/src/week11/Dog.java
+++ week11/src/week11/Dog.java
@@ -1,16 +1,23 @@
 package week11;
 
 public class Dog extends Animal {
+//Animal 클래스 상속
 	public Dog() {
+	//생성자 선언
 		this.kind = "포유류";
+		//종류 필드에 넣는다
 	}
 	@Override
 	public void sound() {
+	//부모 메소드 오버라이드
 		System.out.println("멍멍");
+		//출력
 	}
 	
 	public void dogLife() {
+	//강아지 설명 메소드
 		System.out.println("Dog 평균 연령은 약15~20년 입니다");
+		//출력
 	}
 	
 	
