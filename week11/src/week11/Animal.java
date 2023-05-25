b26e43841b1fb83a57e6b74775172c5e08f4014a
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 20:35:36 +0900

    Update Animal.java

--- week11/src/week11/Animal.java
+++ week11/src/week11/Animal.java
@@ -2,10 +2,14 @@ package week11;
 
 public abstract class Animal {
 	public String kind;
+	//동물의 종류 필드
 	
 	public void breathe() {
+	//숨쉬기 메소드
 		System.out.println("숨을 쉽니다");
+		//출력
 	}
 	
 	public abstract void sound();
+	//추상 메소드
 }	
