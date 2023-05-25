e49d3203cb5a6b6442db3a7f1e2713f242156a3e
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 21:15:01 +0900

    Update Pokemon.java

--- week11/src/week11/Pokemon.java
+++ week11/src/week11/Pokemon.java
@@ -2,12 +2,16 @@ package week11;
 
 public abstract class Pokemon {
 	public void introduce() {
+	//소개 하는 메소드
 		name();
 		attack();
 		passive();
 	}
 
 	public abstract void name();
+	//추상 메소드1
 	public abstract void attack();
+	//추상 메소드2
 	public abstract void passive();
+	//추상 메소드3 
 }
