7d3ec745e15e6b23f68a79de4b5003cb5b8b8b70
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:56:30 +0900

    Update ClassD.java

--- week10/src/week10/ClassD.java
+++ week10/src/week10/ClassD.java
@@ -2,9 +2,11 @@ package week10;
 
 public class ClassD {
 	public int field1;
+	//public 필드
 	int field2;
-	//default
+	//default 필드
 	private int field3;
+	//private 필드
 	
 	public ClassD() {
 		field1 = 10;
@@ -13,10 +15,13 @@ public class ClassD {
 		method1();
 		method2();
 		method3();
+		//클래스 내에서는 private까지 접근 가능
 	}
 	public void method1() {}
+	//public 메소드
 	void method2() {}
-	//default
+	//default 메소드
 	private void method3() {}
+	//private 메소드
 	
 }
