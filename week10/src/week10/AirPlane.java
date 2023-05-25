63ef33650e6495e7434abb4ef60fc3518c4f424d
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:59:21 +0900

    Update AirPlane.java

--- week10/src/week10/AirPlane.java
+++ week10/src/week10/AirPlane.java
@@ -2,14 +2,20 @@ package week10;
 
 public class AirPlane {
 	public void land() {
+	//착륙 메소드
 		System.out.println("착륙합니다.");
+		//출력
 	}
 	
 	public void takeOff() {
+	//이륙 메소드
 		System.out.println("이륙합니다.");
+		//출력
 	}
 	
 	public void fly() {
+	//비행 메소드
 		System.out.println("일반비행합니다");
+		//출력
 	}
 }
