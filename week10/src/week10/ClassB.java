56dd8d28814f800854aa6a750f0786d4091847c9
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:54:34 +0900

    Update ClassB.java

--- week10/src/week10/ClassB.java
+++ week10/src/week10/ClassB.java
@@ -2,7 +2,9 @@ package week10;
 
 public class ClassB {
 	ClassA c1 = new ClassA(true);
+	//public 생성자
 	ClassA c2 = new ClassA(10);
+	//default 생성자
 	//ClassA c3 = new ClassA("문자열");
 	//패키지가 같아도 private는 호출 불가
 }
