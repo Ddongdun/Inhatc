3cb622700b48c9e6e5e50c84b6573ddb3d3fe416
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 21:12:50 +0900

    Update CarEx.java

--- week11/src/week11/CarEx.java
+++ week11/src/week11/CarEx.java
@@ -3,12 +3,19 @@ package week11;
 public class CarEx {
 	public static void main(String[] args) {
 		Car car = new Car();
+		//Car 클래스 생성자 선언
 		car.tire = new Tire();
+		//Car 클래스 내에 선언된 Tire 생성자로 선언
 		car.run();
+		//car 메소드 실행
 		
 		car.tire = new HankookTire();
+		//한국 타이어로 형변환
 		car.run();
+		//한국 타이어의 run 메소드 실행
 		car.tire = new KumhoTire();
+		//금호 타이어로 형변환
 		car.run();
+		//금호 타이어의 run 메소드 실행 
 	}
 }
