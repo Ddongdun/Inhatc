94bb8d9c79e13a42d200461d5e58cf7210749cd9
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:36:41 +0900

    Update StaticBlock.java

--- week9/src/week9/StaticBlock.java
+++ week9/src/week9/StaticBlock.java
@@ -17,14 +17,19 @@ public class StaticBlock {
 		method2();
 	}
 	static void method3() {
-//		this.field1 = 10;
-//		this.method1();
+		//this.field1 = 10;
+		//this.method1();
+		//인스턴스 멤버는 객체에 소속된 멤버이기에 객체 없이 사용 불가
+		//this 사용 불
 		
 		field2 = 10;
 		method2();
+		//정적 멤버는 클래스에 소속된 멤버이기에 사용 가능
 		
 		StaticBlock sb = new StaticBlock();
+		//인스턴스 멤버를 사용하기 위해 객체 생성
 		sb.field1 = 10;
 		sb.method1();
+		//참조변수로 접근
 	}
 }
