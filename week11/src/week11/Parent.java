6794c99c36575a0cac9ed77e2282ade17c8afec3
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 20:51:13 +0900

    Update Parent.java

--- week11/src/week11/Parent.java
+++ week11/src/week11/Parent.java
@@ -2,11 +2,16 @@ package week11;
 
 public class Parent {
 	String field;
+	//필드 선
 	
 	public void method1() {
+	//메소드 1
 		System.out.println("Parent method1");
+		//출력
 	}
 	public void method2() {
+	//메소드 2
 		System.out.println("Parent method2");
+		//출력
 	}
 }
