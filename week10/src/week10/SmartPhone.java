86eec97e0b0987741a26e0e6f5b0d2a71eb9bfb0
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 20:10:54 +0900

    Update SmartPhone.java

--- week10/src/week10/SmartPhone.java
+++ week10/src/week10/SmartPhone.java
@@ -1,22 +1,33 @@
 package week10;
 
 public class SmartPhone extends Phone{
+//Phone 클래스를 상속받는다
 	private boolean wifi;
+	//와이파이 상태를 알려주는 필드
 	public SmartPhone(String model, String color) {
+	//생성자 선언
 		super(model, color);
+		//부모의 model과 color를 받는다
 		//this.model = model;
 		//this.color = color;
 		System.out.println("SmartPhone 생성자 호출");
+		//출력
 	}
 	public boolean isWifi() {
+	//wifi의 getter
 		return wifi;
+		//같은 boolean타입으로 선언해 true or false 값 리턴
 	}
 	public void setWifi(boolean wifi) {
+	//wifi의 setter
 		this.wifi = wifi;
+		//매개변수 값으로 설정
 	}
 	
 	public void internet() {
+	//인터넷 메소드
 		System.out.println("인터넷 연결");
+		//출력
 	}
 	
 	
