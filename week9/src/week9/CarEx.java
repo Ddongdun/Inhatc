2564e228cd98f5588975ea8a81d4e90395b23ede
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-05-25 19:24:27 +0900

    Update CarEx.java

--- week9/src/week9/CarEx.java
+++ week9/src/week9/CarEx.java
@@ -3,13 +3,19 @@ package week9;
 public class CarEx {
 	public static void main(String[] args) {
 		Car mycar = new Car();
+		//Car 클래스 생성자 선언
 		mycar.setGas(5);
+		//연료를 5만큼 준다
 		
 		if(mycar.isLeftGas()) {
+		//연료가 있다면
 			System.out.println("출발합니다.");
+			//출력
 		}
 		
 		mycar.run();
+		//달린다
 		System.out.println("프로그램 종료");
+		//종료
 	}
 }
