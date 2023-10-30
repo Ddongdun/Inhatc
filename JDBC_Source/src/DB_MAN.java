6cc736132bd7ba464d381d5cc52ed41436b934d0
Magic_Nuts <49547459+Ddongdun@users.noreply.github.com>
2023-10-30 15:49:42 +0900

    Add files via upload

---
+++ JDBC_Source/src/DB_MAN.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.*;
+import java.io.*;
+/**
+ *
+ * @author SDJ
+ */
+public class DB_MAN {
+    String strDriver = "sun.jdbc.odbc.JdbcOdbcDriver";
+    String strURL = "jdbc:odbc:Automobile64";
+    
+    Connection DB_con;
+    Statement DB_stmt;
+    ResultSet DB_rs;
+    
+    public void dbOpen() throws IOException{
+        try{
+            Class.forName(strDriver);
+            DB_con = DriverManager.getConnection(strURL);
+            DB_stmt = DB_con.createStatement();
+        }catch(Exception e){
+            System.out.println("SQLException : " + e.getMessage());
+        }
+    }
+    
+    public void dbClose() throws IOException{
+        try{
+            DB_stmt.close();
+            DB_con.close();
+        }catch(SQLException e){
+            System.out.println("SQLException : " + e.getMessage());
+        }
+    }
+}
